d0d4c6fa2f8bcdfb648e75a0d1a7dace1cc1acf2
Carlos Butron <beiosdev@icloud.com>
2016-03-27 21:18:06 +0200

    Refactorized

--- Yorkie/MainCollectionCell.h
+++ Yorkie/MainCollectionCell.h
@@ -9,27 +9,24 @@
 #import <UIKit/UIKit.h>
 
 @class MainCollectionCell;
+
 @protocol YorkieCollectionCellDelegate
--(void)tableCellDidSelect:(UITableViewCell *)cell;
+- (void)tableCellDidSelect:(UITableViewCell *)cell;
 @end
 
 @interface MainCollectionCell : UICollectionViewCell<UITableViewDataSource, UITableViewDelegate>
+
 @property (weak, nonatomic) IBOutlet UIImageView *imageYorkie;
-@property (weak, nonatomic) IBOutlet UILabel *labelYorkie;
 @property (weak, nonatomic) IBOutlet UIView *viewYorkie;
-
-
 @property (weak, nonatomic) IBOutlet UIView *detailValuesView;
+@property (weak, nonatomic) IBOutlet UILabel *labelYorkie;
 @property (weak, nonatomic) IBOutlet UILabel *ageYorkieLabel;
 @property (weak, nonatomic) IBOutlet UILabel *weightYorkieLabel;
 @property (weak, nonatomic) IBOutlet UILabel *genderYorkieLabel;
-
-
-@property(strong,nonatomic) NSArray *cellData;
-@property(weak,nonatomic) id<YorkieCollectionCellDelegate> delegate;
-@property (weak, nonatomic) IBOutlet UITableView *tableView;
+@property (strong,nonatomic) NSArray *cellData;
 @property (strong, nonatomic) NSString *cellIdentifier;
+@property (weak,nonatomic) id<YorkieCollectionCellDelegate> delegate;
+@property (weak, nonatomic) IBOutlet UITableView *tableView;
 @property NSInteger yorkieIdentifier;
 
 @end
-
