31e200ab89a6808784477864f5dc2e5d36f9978f
Carlos Butron <beiosdev@icloud.com>
2016-04-08 13:07:23 +0200

    Refactorized

--- Yorkie/RoutineDelete.m
+++ Yorkie/RoutineDelete.m
@@ -15,7 +15,6 @@
 
 + (void)routineDelete:(NSInteger)idYorkie withRoutineNumber:(NSInteger)routineNumber {
  
-    
     NSArray *docPaths = NSSearchPathForDirectoriesInDomains(NSLibraryDirectory, NSUserDomainMask, YES);
     NSString *documentsDir = [docPaths objectAtIndex:0];
     NSString *dbPath = [documentsDir stringByAppendingPathComponent:@"yorkie.sqlite"];
@@ -23,45 +22,30 @@
     FMDatabase *database = [FMDatabase databaseWithPath:dbPath];
     [database open];
 
-    
     int idRoutine=0;
     //get the idYorkie
     FMResultSet *results = [database executeQuery:@"SELECT idRoutine FROM routine WHERE idYorkie= ? and idRoutineType= ?", [NSString stringWithFormat:@"%ld", (long)idYorkie ], [NSString stringWithFormat:@"%ld", (long)routineNumber], nil];
     
     while([results next]) {
-        
         idRoutine = [results intForColumn:@"idRoutine"];
-        
     }
     
     if (!results) {
         NSLog(@"%s: insert error: %@", __FUNCTION__, [database lastErrorMessage]);
-        
         // do whatever you need to upon error
     }
-    
-    
-    
-    
+
     //DELETE ROUTINES
     BOOL successRoutine = [database executeUpdate:@"DELETE FROM routine WHERE idYorkie= ? and idRoutineType= ?", [NSString stringWithFormat:@"%ld", (long)idYorkie ], [NSString stringWithFormat:@"%ld", (long)routineNumber]];
-    
-    
+
     if (!successRoutine) {
         NSLog(@"%s: insert error: %@", __FUNCTION__, [database lastErrorMessage]);
-        
         // do whatever you need to upon error
     }
 
-    
     [database close];
-    
     //delete notification of this routine if exists
     [NotificationDelete notificationDelete:idRoutine];
-    
-    
-
-    
 }
 
 @end
