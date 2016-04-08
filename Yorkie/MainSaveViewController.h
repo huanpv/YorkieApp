de24d1496bdb3e841a698079af5330e78fc5f525
Carlos Butron <beiosdev@icloud.com>
2016-04-08 12:21:22 +0200

    Refactorized

--- Yorkie/MainSaveViewController.h
+++ Yorkie/MainSaveViewController.h
@@ -9,25 +9,18 @@
 #import <UIKit/UIKit.h>
 #import "UITextFieldLimit.h"
 
-
 //this protocol-delegate send an alert to reload collectionCell detail view when
 //user edit data
 @protocol SaveYorkieViewControllerEditDelegate
 -(void)reloadDetail;
 @end
 
-
 @interface MainSaveViewController : UIViewController<UITextFieldDelegate, UIPickerViewDataSource,UIPickerViewDelegate, UIActionSheetDelegate, UIImagePickerControllerDelegate, UINavigationControllerDelegate, UIAlertViewDelegate, UITextFieldLimitDelegate>
 
 @property(retain,nonatomic) id<SaveYorkieViewControllerEditDelegate> delegate;
-
-
 //get from the last view if we need to delete or edit Yorkie
 @property BOOL save;
 @property BOOL edit;
 @property NSInteger idYorkie;
-
-
-
 
 @end
