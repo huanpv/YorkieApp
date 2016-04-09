2c0edcacb0deebf64b0847e38ebf4b30a98da461
Carlos Butron <beiosdev@icloud.com>
2016-04-09 18:19:40 +0200

    Refactorized

--- Yorkie/RoutineViewController.m
+++ Yorkie/RoutineViewController.m
@@ -17,9 +17,7 @@
 
 @interface RoutineViewController ()
 
-
 @property (nonatomic, strong) Routine *routine;
-
 @property (weak, nonatomic) IBOutlet UICollectionView *collectionView;
 
 @end
@@ -28,13 +26,7 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-
-    
-    // Do any additional setup after loading the view.
 }
-
-
-
 
 //set custom navigation bar
 - (void)viewWillAppear:(BOOL)animated {
@@ -43,10 +35,7 @@
     [self styleNavBar];
     [self databaseRoutineDidOpen];
     [self.collectionView reloadData];
-
 }
-
-
 
 //custom navigation bar
 - (void)styleNavBar {
@@ -60,20 +49,13 @@
     self.navigationItem.rightBarButtonItem = anotherButton;
 }
 
-
-
-
-
 - (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
-    // Dispose of any resources that can be recreated.
 }
-
 
 - (IBAction)actionEditButton:(id)sender {
     
     UIStoryboard *storyboard = [UIStoryboard storyboardWithName:@"Main" bundle:nil];
-    
     RoutineSaveViewController *dVC = [storyboard instantiateViewControllerWithIdentifier:@"SaveEditRoutine"];
     
     dVC.edit = YES;
@@ -84,45 +66,33 @@
     
     UINavigationController *navController = [[UINavigationController alloc] initWithRootViewController:dVC];
     [self presentViewController:navController animated:YES completion:nil];
-    
 }
-
-
-
 
 //********COLLECTION VIEW***********//
 #pragma mark - UICollectionView Methods
 
-
 //elements of Collection View
 - (NSInteger)collectionView:(UICollectionView *)collectionView numberOfItemsInSection:(NSInteger)section {
-    
-    
     return 1;
-    
-    
 }
 
-
 //table highlight when tap photo disallow
--(BOOL)collectionView:(UICollectionView *)collectionView shouldSelectItemAtIndexPath:(NSIndexPath *)indexPath {
+- (BOOL)collectionView:(UICollectionView *)collectionView shouldSelectItemAtIndexPath:(NSIndexPath *)indexPath {
     return YES;
 }
 
-
 //table highlight when tap photo disallow
--(BOOL)collectionView:(UICollectionView *)collectionView shouldHighlightItemAtIndexPath:(NSIndexPath *)indexPath {
+- (BOOL)collectionView:(UICollectionView *)collectionView shouldHighlightItemAtIndexPath:(NSIndexPath *)indexPath {
     return NO;
 }
 
--(void)collectionView:(UICollectionView *)collectionView didSelectItemAtIndexPath:(NSIndexPath *)indexPath {
+- (void)collectionView:(UICollectionView *)collectionView didSelectItemAtIndexPath:(NSIndexPath *)indexPath {
     // NOTE: This is called, as long as both shouldSelectItemAtIndexPath: AND shouldHighlightItemAtIndexPath: return YES. If either returns NO, the cell is not actually selected.
     NSLog(@"Did select collection view cell: %@", indexPath);
 }
 
-
 //get the cell of Collection View
--(UICollectionViewCell *)collectionView:(UICollectionView *)collectionView cellForItemAtIndexPath:(NSIndexPath *)indexPath {
+- (UICollectionViewCell *)collectionView:(UICollectionView *)collectionView cellForItemAtIndexPath:(NSIndexPath *)indexPath {
     
     RoutineCollectionCell * yorkieCollectionCell = [collectionView dequeueReusableCellWithReuseIdentifier:self.cellIdentifier forIndexPath:indexPath];
     
@@ -138,7 +108,6 @@
     yorkieCollectionCell.imageRoutine.image = [UIImage imageNamed:self.routineImage];
     yorkieCollectionCell.routineLabel.text = NSLocalizedString(self.routineDescription, nil);
     yorkieCollectionCell.nextLabel.text = NSLocalizedString(@"Next", nil);
-    
     
     //fix to spanish language
     if (([yorkieCollectionCell.routineLabel.text isEqualToString:@"Peluquería"]) || ([yorkieCollectionCell.routineLabel.text isEqualToString:@"Vacunación"]) || ([yorkieCollectionCell.routineLabel.text isEqualToString:@"Pastilla"]) || ([yorkieCollectionCell.routineLabel.text isEqualToString:@"Medicina"])) {
@@ -162,16 +131,11 @@
                 yorkieCollectionCell.routineLabel.font = [routineFont fontWithSize:32]; //if we need to change only the font size
             }
                 break;
-                
             default:
                 break;
         }
-        
-
-        
     }
 
-    
     //cast routine.startDate string into NSDate
     NSDateFormatter *dateFormat = [[NSDateFormatter alloc] init];
     [dateFormat setDateFormat:@"dd/MM/yyyy"];
@@ -181,7 +145,6 @@
     if (self.routine.frecuency==1) {
         yorkieCollectionCell.dateFrequencyLabel.text = [NSString stringWithFormat:@"%@ %ld %@", NSLocalizedString(@"Repeat every", nil), (long)self.routine.frecuency, NSLocalizedString(@"day", nil)];
     } else {
-        
         if (self.routine.frecuency==365) {
             yorkieCollectionCell.dateFrequencyLabel.text = [NSString stringWithFormat:@"%@", NSLocalizedString(@"Repeat every year", nil)];
         } else {
@@ -189,36 +152,20 @@
         }
     }
     
-    
-    
-
     yorkieCollectionCell.dateCommentLabel.text = self.routine.name;
     yorkieCollectionCell.dateNextLabel.text = [DateofNextEvent age:self.routine.nextDate];
     
-    
-    
-    
     //TAP on routine details
     UITapGestureRecognizer *tapRoutineValues=[[UITapGestureRecognizer alloc] initWithTarget:self action:@selector(cellTap:)];
-
-    
-    
     [yorkieCollectionCell.routineValuesView addGestureRecognizer:tapRoutineValues];
 
-    
-    
-    
     return yorkieCollectionCell;
-    
 }
 
-
 //tap photo yorkie or label name
--(void)cellTap:(UISwipeGestureRecognizer *)gesture
-{
+- (void)cellTap:(UISwipeGestureRecognizer *)gesture {
     
     UIStoryboard *storyboard = [UIStoryboard storyboardWithName:@"Main" bundle:nil];
-    
     RoutineSaveViewController *dVC = [storyboard instantiateViewControllerWithIdentifier:@"SaveEditRoutine"];
     
     dVC.edit = YES;
@@ -229,29 +176,22 @@
     
     UINavigationController *navController = [[UINavigationController alloc] initWithRootViewController:dVC];
     [self presentViewController:navController animated:YES completion:nil];
-    
 }
-
-
 
 //get the routine data to show in routine view
 - (void)databaseRoutineDidOpen  {
-    
     
     NSArray *docPaths = NSSearchPathForDirectoriesInDomains(NSLibraryDirectory, NSUserDomainMask, YES);
     NSString *documentsDir = [docPaths objectAtIndex:0];
     NSString *dbPath = [documentsDir stringByAppendingPathComponent:@"yorkie.sqlite"];
     
-    
     FMDatabase *database = [FMDatabase databaseWithPath:dbPath];
     [database open];
     FMResultSet *results = [database executeQuery:@"SELECT routine.idRoutine, routine.idYorkie, routine.idRoutineType, routine.comment, routine.startDate, routine.frequency, routineType.image, routineType.description FROM routine, routineType where ? = routine.idYorkie and ? = routine.idRoutine and routine.idRoutineType = routineType.idRoutineType", [NSString stringWithFormat:@"%ld",(long)self.idYorkie], [NSString stringWithFormat:@"%ld",(long)self.idRoutine]];
     
-
     self.routine= [[Routine alloc] init];
     
     while([results next]) {
-
         self.routine.yorkieID = [results intForColumn:@"idYorkie"];
         self.routine.routineID = [results intForColumn:@"idRoutine"];
         self.routine.routineTypeID = [results intForColumn:@"idRoutineType"];
@@ -267,19 +207,10 @@
         NSDate *date = [dateFormat dateFromString: self.routine.startDate];
         
         self.routine.nextDate = [DateofNextEvent nextEventDate:date withFrequency:self.routine.frecuency];
-
-        
     }
     
     [database close];
-    
-    
 }
-
-
-
-
-
 
 #pragma mark - CollectionView layout
 
@@ -310,7 +241,6 @@
     return mElementSize;
 }
 
-
 //vertical separation between cells
 - (CGFloat)collectionView:(UICollectionView *)collectionView layout:(UICollectionViewLayout*)collectionViewLayout minimumInteritemSpacingForSectionAtIndex:(NSInteger)section {
     
@@ -327,13 +257,11 @@
         case 7: //iPhone 6 Plus
             return 0.0;
             break;
-            
         default: //default
             return 0.0;
             break;
     }
 }
-
 
 //horizontal separation between cells
 - (CGFloat)collectionView:(UICollectionView *)collectionView layout:(UICollectionViewLayout*)collectionViewLayout minimumLineSpacingForSectionAtIndex:(NSInteger)section {
@@ -351,20 +279,14 @@
         case 7: //iPhone 6 Plus
             return 25.0;
             break;
-            
         default: //default
             return 0.0;
             break;
     }
-    
-    
 }
-
-
 
 // Layout: Set Edges
 - (UIEdgeInsets)collectionView:(UICollectionView *)collectionView layout:(UICollectionViewLayout*)collectionViewLayout insetForSectionAtIndex:(NSInteger)section {
-    
     
     switch (self.iphoneModel) {
         case 4: //iPhone 4
@@ -379,17 +301,13 @@
         case 7: //iPhone 6 plus
             return UIEdgeInsetsMake(0,12,0,0);
             break;
-            
         default: //default
             return UIEdgeInsetsMake(0,0,0,0);// top, left, bottom, right
             break;
     }
-    
 }
 
-
-- (CGSize)collectionView:(UICollectionView *)collectionView layout:(UICollectionViewLayout*)collectionViewLayout referenceSizeForFooterInSection:(NSInteger)section
-{
+- (CGSize)collectionView:(UICollectionView *)collectionView layout:(UICollectionViewLayout*)collectionViewLayout referenceSizeForFooterInSection:(NSInteger)section {
     switch (self.iphoneModel) {
         case 4: //iPhone 4
             return CGSizeMake(19, 0);
@@ -408,14 +326,6 @@
             return CGSizeMake(0, 0);
             break;
     }
-    
-    
 }
-
-
-
-
-
-
 
 @end
